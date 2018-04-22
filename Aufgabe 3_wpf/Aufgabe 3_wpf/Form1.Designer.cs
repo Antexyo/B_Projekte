705f52cdbbdcce567c8c6f0dc1fdb398e4d11dde
antexyo <benjamin.diehm@hotmail.de>
2018-04-23 00:15:27 +0200

    added afg 3

---
+++ Aufgabe 3_wpf/Aufgabe 3_wpf/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Aufgabe_3_wpf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btPruefziffer = new System.Windows.Forms.Button();
+            this.tbxSeriennummer = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblSeriennummer = new System.Windows.Forms.Label();
+            this.lblPruefziffer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btPruefziffer
+            // 
+            this.btPruefziffer.Location = new System.Drawing.Point(429, 250);
+            this.btPruefziffer.Name = "btPruefziffer";
+            this.btPruefziffer.Size = new System.Drawing.Size(183, 20);
+            this.btPruefziffer.TabIndex = 0;
+            this.btPruefziffer.Text = "Prüfziffer berechnen";
+            this.btPruefziffer.UseVisualStyleBackColor = true;
+            this.btPruefziffer.Click += new System.EventHandler(this.btPruefziffer_Click);
+            // 
+            // tbxSeriennummer
+            // 
+            this.tbxSeriennummer.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbxSeriennummer.Location = new System.Drawing.Point(208, 250);
+            this.tbxSeriennummer.MaxLength = 2000;
+            this.tbxSeriennummer.Name = "tbxSeriennummer";
+            this.tbxSeriennummer.Size = new System.Drawing.Size(183, 20);
+            this.tbxSeriennummer.TabIndex = 1;
+            // 
+            // lblSeriennummer
+            // 
+            this.lblSeriennummer.AutoSize = true;
+            this.lblSeriennummer.Location = new System.Drawing.Point(205, 215);
+            this.lblSeriennummer.Name = "lblSeriennummer";
+            this.lblSeriennummer.Size = new System.Drawing.Size(74, 13);
+            this.lblSeriennummer.TabIndex = 2;
+            this.lblSeriennummer.Text = "Seriennummer";
+            // 
+            // lblPruefziffer
+            // 
+            this.lblPruefziffer.AutoSize = true;
+            this.lblPruefziffer.Location = new System.Drawing.Point(208, 304);
+            this.lblPruefziffer.Name = "lblPruefziffer";
+            this.lblPruefziffer.Size = new System.Drawing.Size(0, 13);
+            this.lblPruefziffer.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 503);
+            this.Controls.Add(this.lblPruefziffer);
+            this.Controls.Add(this.lblSeriennummer);
+            this.Controls.Add(this.tbxSeriennummer);
+            this.Controls.Add(this.btPruefziffer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btPruefziffer;
+        private System.Windows.Forms.TextBox tbxSeriennummer;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblSeriennummer;
+        private System.Windows.Forms.Label lblPruefziffer;
+    }
+}
+
---
+++ Aufgabe 3_wpf/Aufgabe 3_wpf/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aufgabe_3_wpf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btPruefziffer_Click(object sender, EventArgs e)
+        {
+
+            // Einlesen der Serienummer als String
+            string seriennummerggbn = tbxSeriennummer.Text;
+            
+            // Methoden Start
+            int Pruefziffer(string snummer)
+            {
+                int restwert, quersumme, ergebnis;
+                int char1 = 0;
+                int vorergebnis = 0;
+
+                char[] alphabet = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+                // Umwandeln des Seriennumme Arrays in Chararray
+                char[] seriennummer = snummer.ToCharArray();
+                int[] intarray = new int[seriennummer.Length -1];
+
+                // Herausfinden der Zahl die zu der Stelle des Alphabets gehört
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    if (seriennummer [0] == alphabet [i])
+                    {
+                        // Char Wert vom Alphabet
+                        char1 = i + 1;
+                        // Falls Wert >= 10 splitte für Quersumme
+                        if (char1 >= 10)
+                        {
+                            int charsub1 = char1 / 10;
+                            int charsub2 = char1 % 10;
+                            char1 = charsub1 + charsub2;
+                        }
+                        
+                        
+                    }
+                }
+                // Conver to Int Array
+                for (int i = 1; i < seriennummer.Length  ; i++)
+                {
+                    intarray[i-1] = Convert.ToInt32(seriennummer[i].ToString());
+                }
+                // Zusammenzählen ohne Buchstaben
+                for (int i = 0; i < intarray.Length; i++)
+                {
+                    vorergebnis = vorergebnis + intarray[i];
+                }
+                // Finales Zusammenzählen
+                quersumme = vorergebnis + char1;
+                // Rest der Berechnungen
+                restwert = quersumme % 9;
+                if (8 - restwert == 0)
+                {
+                    ergebnis = 9;
+                }
+                else
+                {
+                    ergebnis = 8 - restwert;
+                }
+                return ergebnis;
+            }
+            // Ausgabe in Label , Aufrufen der Methode
+            lblPruefziffer.Text = Pruefziffer(seriennummerggbn).ToString();
+        }
+    }
+}
