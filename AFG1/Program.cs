d40ca97d4e802219640b8da27e4c0f05c03ab947
antexyo <benjamin.diehm@hotmail.de>
2018-04-23 23:26:47 +0200

    added 1 and 3 new

---
+++ AFG1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+class WallisschesProdukt
+{
+
+    public static double berechneProdukt(int n)
+    {
+        double ergebnis = 1;
+
+
+        for (double i = 2; i <= (2*n); i +=2)
+        {
+            ergebnis *= (i / (i - 1)) * (i / (i + 1));
+        }
+
+        return ergebnis *2 ;
+    }
+
+
+    public static void Main()
+    {
+        Console.WriteLine("Geben Sie den Faktor ein: ");
+        Console.WriteLine("Das Produkt ist: " + WallisschesProdukt.berechneProdukt(Convert.ToInt32(Console.ReadLine()))); 
+        Console.ReadKey();
+    }
+}
---
+++ Afg3_real/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class prfziffer
+{
+    public static int pruefziffer_berechnen(char [] seriennummer)
+    {
+        int pruefziffer, quersumme = 0;
+        char[] alphabet = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            if (seriennummer[0] == alphabet[i])
+            {
+                quersumme = (i + 1) % 10 + (i + 1) / 10;
+
+
+            }
+        }
+
+        for (int i = 1; i < seriennummer.Length; i++)
+        {
+            quersumme += Convert.ToInt32(seriennummer[i].ToString());
+        }
+
+        pruefziffer = quersumme % 9;
+
+        if (pruefziffer == 0)
+        {
+            pruefziffer = 9;
+        }
+        else
+        {
+            pruefziffer = 8 - pruefziffer;
+        }
+        return pruefziffer;
+    }
+
+    public static void Main()
+    {
+
+        char[] seriennummer = new char[] { 'V', '0', '2', '3', '8', '7', '0', '4', '0', '0', '3' };
+
+        Console.WriteLine("Prüfziffer: " + prfziffer.pruefziffer_berechnen(seriennummer));
+        Console.ReadKey();
+    }
+}
